a152dc45753736d8f6c43e137f122f7ff9b9344a
Jesse Patrick <jesse.patrick@mittera.com>
2023-04-04 15:55:17 -0500

    End of Section 2

---
+++ DatingAppAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingAppAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
